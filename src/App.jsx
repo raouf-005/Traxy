097549c518b4728c0bc5074d6fb9cde2f17f68a8
raoufard <a_derardja@estin.dz>
2024-07-17 10:58:50 +0100

    router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,59 @@
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import './App.css'
-import Landing from './pages/Landing';
-import Gps from './pages/Gps';
-import Solar from './pages/Solar';
-import { Suspense } from 'react';
-import Camera from './pages/Camera';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Landing from "./pages/Landing";
+import Gps from "./pages/Gps";
+import Solar from "./pages/Solar";
+import { Suspense } from "react";
+import Camera from "./pages/Camera";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+      errorElement: (
+        <div className="flex items-center justify-center text-7xl p-12">
+          404 Not Found
+        </div>
+      ),
+    },
+    {
+      path: "/gps",
+      element: <Gps />,
+      errorElement: (
+        <div className="flex items-center justify-center text-7xl p-12">
+          404 Not Found
+        </div>
+      ),
+    },
+    {
+      path: "/solar",
+      element: <Solar />,
+      errorElement: (
+        <div className="flex items-center justify-center text-7xl p-12">
+          404 Not Found
+        </div>
+      ),
+    },
+    {
+      path: "/camera",
+      element: <Camera />,
+      errorElement: (
+        <div className="flex items-center justify-center text-7xl p-12">
+          404 Not Found
+        </div>
+      ),
+    },
+  ]);
 
-
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/*',
-        element: <Landing />,
-        errorElement: <div>404 Not Found</div>
-      },
-      {
-        path: '/gps',
-        element: <Gps />,
-        errorElement: <div>404 Not Found</div>
-      },{
-        path: '/solar',
-        element: <Solar />,
-        errorElement: <div>404 Not Found</div>
-      },{
-        path: '/camera',
-        element: <Camera />,
-        errorElement: <div>404 Not Found</div>
-      }
-
-    ]
+  return(
+    
+      <RouterProvider router={router} />
   );
 
-  return (
-    
-    <RouterProvider router={router} />
-  )
 }
 
-export default App
-//    </Suspense> */
-// <Suspense fallback={<div className='flex justify-center items-center text-7xl'>Loading...</div>}> */}
+export default App;
+
+//<Suspense fallback={<div className='flex justify-center items-center text-7xl'>Loading...</div>}>
+//</Suspense>
