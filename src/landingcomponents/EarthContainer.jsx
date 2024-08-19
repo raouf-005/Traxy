0dc45940479559f18fb4a1eb46fb087370a697e0
raoufard <a_derardja@estin.dz>
2024-08-19 09:02:27 +0100

    test webgl support

--- src/landingcomponents/EarthContainer.jsx
+++ src/landingcomponents/EarthContainer.jsx
@@ -2,10 +2,30 @@ import React from "react";
 import "../App.css";
 import EarthLayout from "./EarthLayout";
 
+const detectWebGLContext = () => {
+  // Create canvas element. The canvas is not added to the
+  // document itself, so it is never displayed in the
+  // browser window.
+  const canvas = document.createElement("canvas");
+
+  // Get WebGLRenderingContext from canvas element.
+  const gl =
+    canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+  // Report the result.
+  return gl instanceof WebGLRenderingContext;
+};
+
 export default function EarthContainer() {
   return (
     <div className="flex flex-row justify-center px-24 text-white  bg-custom   py-12">
-      <EarthLayout />
+      {detectWebGLContext() ? (
+        <EarthLayout />
+      ) : (
+        <p className="text-center max-w-sm text-5xl">
+          WebGL is not supported on your browser
+        </p>
+      )}
       <div className="flex-1 flex justify-center items-top lg:mt-40">
         <p className="text-center  leading-[47px] max-w-md lg:max-w-3xl text-lg  ">
           Traxy excels in providing support and assistance at all stages of a
